2ef93956ddced69995a03feceff0d79dc5c7a908
Sarthak Rana <rsarthak2845@gmail.com>
2026-02-19 15:45:15 +0530

    feat(frontend): initialize frontend project with Next.js, TypeScript, and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Created postcss.config.mjs for Tailwind CSS integration
    - Added SVG assets for UI components
    - Configured TypeScript with tsconfig.json for strict type checking and module resolution

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-4xl text-amber-500 font-extrabold">Hi this is our fucking project</h1>
+        </div>
+    </div>
+  );
+}
